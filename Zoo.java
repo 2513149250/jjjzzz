7941a7f24632eb36632f0a8f9088c17844c1e848
2513149250 <43224857+2513149250@users.noreply.github.com>
2018-09-27 08:28:43 +0800

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -1,26 +1,29 @@
 import  java.awt.print.Printable ;
+public class Father{
+//父类有一个打孩子方法
+public void hitChild(){
+}
+}
 
-公共 类 动物园 {
 
 
-	public  Zoo（String  name，String  eat，String  cry）{
-		super（）;
-		name = name;
-	
-	}
-	
-	public  void  print（）{
-		系统。出。println（name + “：” + eat + “  ” + cry）;
-	}
-	public  static  void  main（String [] args）{
-		狗的狗=  新的 狗（“狗”，“二狗子”，“三狗”，3）;
-		狗。打印（）;
-		Cat cat =  new  Cat（“ 猫 ”，“二猫”，“三猫”，5）;
-		猫。打印（）;
-		鱼鱼=  新 鱼（“大鱼”，“中鱼”，“LHS”，2）;
-		鱼。打印（）;
-	    Rabiit rabiit =  new  Rabiit（“ 444 ”，“333”，“22”，1）;
-		rabiit 。打印（）;
-	}
 
+
+
+
+
+
+
+
+
+public class Test{
+public static void main(String args[]){
+Father father;
+father = new Son1();
+father.hitChild();
+father = new Son2();
+father.hitChild();
+father = new Son3();
+father.hitChild();
+}
 }
